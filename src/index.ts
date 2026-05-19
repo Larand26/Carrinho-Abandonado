34bed9e7b39eb9d478999b50a356e9cc5027bdcc
Victor de Brito Laranjeira <vbl09012003vbl@gmail.com>
2026-05-19 07:32:46 -0300

    feat: usa o cron para rodar o app a cada 2 horas e limpar o banco de dados às 21h

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,11 @@
 import App from "./App.js";
-
+import nodeCron from "node-cron";
 const app = new App();
-app.start();
+
+nodeCron.schedule("0 10,12,14,16,18 * * *", async () => {
+  app.start();
+});
+
+nodeCron.schedule("0 21 * * *", async () => {
+  await app.clearDatabase();
+});
